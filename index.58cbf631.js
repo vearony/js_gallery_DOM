58b47c1f1380897f2cc176c1c12293993465e183
Veronika Vasyliuk <verka0218@gmail.com>
2024-12-15 23:00:31 +0200

    make build

---
+++ index.58cbf631.js
@@ -0,0 +1,2 @@
+document.addEventListener("click",function(t){var e=t.target.closest("a");if(e){t.preventDefault();var r=e.getAttribute("href"),n=document.getElementById("largeImg");n&&n.setAttribute("src",r)}});
+//# sourceMappingURL=index.58cbf631.js.map
